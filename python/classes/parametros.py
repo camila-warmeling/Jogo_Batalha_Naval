0004d58e0259e8e4dd65f9508264f4515ee21e27
Camila <camila.warmeling04@gmail.com>
2026-01-30 12:44:37 -0300

    chore: cria estrutura inicial do projeto

---
+++ python/classes/parametros.py
@@ -0,0 +1,63 @@
+class Parametros:
+    def __init__(self):
+        self.nivel_dificuldade = None
+        self.quant_submarino = None
+        self.quant_navio = None
+        self.quant_aviao = None
+
+    def nivel_dificuldade_escolhido(self):
+        print('Escolha um nível:\nFácil \nDifícil')
+        while True:
+            nivel_dificuldade = input('Qual o nível de dificuldade escolhido?:')
+            nivel_escolhido = nivel_dificuldade.lower()
+            if nivel_escolhido == "fácil" or nivel_escolhido == "difícil":
+                self.nivel_dificuldade = nivel_escolhido
+                break
+
+    def verificacao_quant_embarcacoes(self, max, embarcacao, quant_embarcacao):
+        print('Mínimo permitido - 1')
+        print(f'Máximo permitido - {max}')
+        self.quant_embarcacao = None
+        while True:
+            quant_embarcacao = input(f'Quantos {embarcacao} você deseja?')
+            if quant_embarcacao.isdigit():
+                quant_embarcacao = int(quant_embarcacao)
+                if quant_embarcacao >=1 and quant_embarcacao <= max:
+                    return quant_embarcacao
+                else:
+                    print(f'Digite somente números de 1 a {max}')
+            else:
+                print('Digite somente números')
+
+    def quantidade_submarino(self):
+        print('Submarino - 1 espaço')
+        verificacao = self.verificacao_quant_embarcacoes(4, 'submarinos', ' ')
+        self.quant_submarino = verificacao
+        
+    def quantidade_navio(self):
+        print('Navio - 1 espaço')
+        verificacao = self.verificacao_quant_embarcacoes(4, 'navios', ' ')
+        self.quant_navio = verificacao
+
+    def quantidade_aviao(self):
+        print('Avião - 1 espaço')
+        verificacao = self.verificacao_quant_embarcacoes(3, 'aviões', ' ')
+        self.quant_aviao = verificacao
+        
+    def mostrar_quantidades(self):
+        print(f'''
+        Nível Selecionado:{self.nivel_dificuldade}
+        Quantidade de Submarinos: {self.quant_submarino}
+        Quantidade de Navios: {self.quant_navio}
+        Quantidade de Aviões: {self.quant_aviao}''')
+
+    def main():
+        parametros = Parametros()
+        parametros.nivel_dificuldade_escolhido()
+        parametros.quantidade_submarino()
+        parametros.quantidade_navio()
+        parametros.quantidade_aviao()
+        parametros.mostrar_quantidades()
+
+print (Parametros.main())
+
