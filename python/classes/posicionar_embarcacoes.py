71e6673c8614acdfb5aae01cce22dfcb5c60eab8
Camila <camila.warmeling04@gmail.com>
2026-02-15 15:40:58 -0300

    feat: cria função para posicionar a embarcacao no mapa

--- python/classes/posicionar_embarcacoes.py
+++ python/classes/posicionar_embarcacoes.py
@@ -1,7 +1,7 @@
 lista_letras = ['a','b','c','d','e','f','g','h','i']
 lista_numeros = [1,2,3,4,5,6,7,8]
 class Posicionar:
-    def __init__(self):
+    def __init__(self, quant_submarino, quant_navio, quant_aviao, tabuleiro):
         self.mapa_computador = None
         self.mapa_usuario = None
      
@@ -20,12 +20,18 @@ class Posicionar:
     def verificar_posicao_vazia(self, posicao):
         self.posicionar_embarcacao(posicao_vazia)
 
-    def posicionar_embarcação(self, posicao):
-                
+    def posicionar_embarcação(self, posicao, tabuleiro, embarcacao, quantidade):
+        coluna = posicao[0] #a
+        linha = posicao[1] #5
+        posicionar_coluna = lista_letras.index(coluna)
+        posicionar_linha = lista_numeros.index(linha)
+        tabuleiro[posicionar_linha][posicionar_coluna]: embarcacao+quantidade
+        quantidade -= 1
+
     def posicionar_submarino(self, quantidade):
         quant_submarino = quantidade
         print('O submarino ocupa 1 espaço no tabuleiro:')
         print(f'Restam {quant_submarino} submarinos!')
         while True:
-            posicao = input('Qual a posição deseja?(Ex:A5):')
+            posicao = input('Qual a posição deseja?(Ex:A5):').strip().lower() #tira os espaços e coloca tudo minusculo
             self.verificar_posicao_embarcacao(posicao)
