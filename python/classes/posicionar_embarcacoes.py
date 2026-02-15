37e398e00aa18805ac31617f40131062f6ecb445
Camila <camila.warmeling04@gmail.com>
2026-02-15 15:06:06 -0300

    feat: cria função para posicionar o submarino

--- python/classes/posicionar_embarcacoes.py
+++ python/classes/posicionar_embarcacoes.py
@@ -1,13 +1,15 @@
-from parametros import Parametros
-
-parametros_embarcacoes = Parametros()
-submarino = parametros_embarcacoes.quant_submarino
-navio = parametros_embarcacoes.quant_navio
-aviao = parametros_embarcacoes.quant_aviao
-
+lista_letras = ['a','b','c','d','e','f','g','h','i']
+lista_numeros = [1,2,3,4,5,6,7,8]
 class Posicionar:
     def __init__(self):
         self.mapa_computador = None
         self.mapa_usuario = None
 
 
+    def posicionar_submarino(self, quantidade):
+        quant_submarino = quantidade
+        print('O submarino ocupa 1 espaço no tabuleiro:')
+        print(f'Restam {quant_submarino} submarinos!')
+        while True:
+            posicao = input('Qual a posição deseja?(Ex:A5):')
+            self.verificar_posicao_embarcacao('submarino', posicao)
