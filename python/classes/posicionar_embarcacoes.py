95cd13fd0dc4d8b9b2a6fec12ccade27ae30847b
Camila <camila.warmeling04@gmail.com>
2026-02-16 14:34:22 -0300

    fix: função para verificar posicao vazia

--- python/classes/posicionar_embarcacoes.py
+++ python/classes/posicionar_embarcacoes.py
@@ -5,33 +5,37 @@ class Posicionar:
         self.mapa_computador = None
         self.mapa_usuario = None
      
-    def verificacao_posicao_correta(self, posicao):
+    def verificacao_posicao_correta(self, posicao, tabuleiro, embarcacao, quantidade):
         if len(posicao) == 2:
             coluna = [0]
             linha = [1]
             if coluna in lista_letras and linha in lista_numeros:
                 posicao_correta = posicao
-                self.verificar_posicao_vazia(posicao_correta)
+                self.verificar_posicao_vazia(posicao_correta, tabuleiro, embarcacao, quantidade, coluna, linha)
             else:
                 print('Digite uma posição válida')
         else:
             print(f'Digite uma posição válida')
 
-    def verificar_posicao_vazia(self, posicao):
-        self.posicionar_embarcacao(posicao_vazia)
-
-    def posicionar_embarcação(self, posicao, tabuleiro, embarcacao, quantidade):
-        coluna = posicao[0] #a
-        linha = posicao[1] #5
+    def verificar_posicao_vazia(self, posicao, tabuleiro, embarcacao, quantidade, coluna, linha):
+        posicao_vazia = tabuleiro[linha][coluna]
+        if posicao_vazia == '•':
+            self.posicionar_embarcacao(posicao_vazia, tabuleiro, embarcacao, quantidade)
+        else:
+            print('a posição não está vazia!')
+            
+    def posicionar_embarcação(self, posicao, tabuleiro, embarcacao, quantidade, coluna, linha):
         posicionar_coluna = lista_letras.index(coluna)
         posicionar_linha = lista_numeros.index(linha)
-        tabuleiro[posicionar_linha][posicionar_coluna]: embarcacao+quantidade
+        tabuleiro[posicionar_linha][posicionar_coluna] = embarcacao+quantidade
         quantidade -= 1
+        return quantidade and tabuleiro
 
-    def posicionar_submarino(self, quantidade):
-        quant_submarino = quantidade
+    def posicionar_submarino(self, quant_submarino, tabuleiro):
+        embarcacao = 's'
+        print(f'Esse é o seu tabuleiro  atual:{tabuleiro}')
         print('O submarino ocupa 1 espaço no tabuleiro:')
         print(f'Restam {quant_submarino} submarinos!')
         while True:
             posicao = input('Qual a posição deseja?(Ex:A5):').strip().lower() #tira os espaços e coloca tudo minusculo
-            self.verificar_posicao_embarcacao(posicao)
+            self.verificar_posicao_embarcacao(posicao, tabuleiro, embarcacao, quant_submarino)
