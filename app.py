31fff9851645e28d6a3c61c85a36d6c97b1d4703
Camila <camila.warmeling04@gmail.com>
2026-02-22 11:11:45 -0300

    feat: cria main e roda o jogo para posicionar o submarino

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from python.classes.parametros import Parametros
 from python.classes.tabuleiro import Tabuleiro
 from python.classes.posicionar_embarcacoes import Posicionar
+
+def main():
+    parametros = Parametros()
+    tabuleiro = Tabuleiro()
+    posicionar = Posicionar(
+        parametros.quant_submarino, 
+        parametros.quant_navio, 
+        parametros.quant_aviao, 
+        tabuleiro.tabuleiro)
+    posicionar.posicionar_submarino()
+
+
+#Dessa forma, a função main só vai rodar se for esse próprio arquivo.
+#Se algum outro arquivo importar o app.py ele não vai rodar automaticamente o jogo.
+if __name__ == '__main__':
+    main()
